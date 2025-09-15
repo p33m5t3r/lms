66c4eb5bcdeaef315c2752a310c15aa6d6ecd9a5
p33m5t3r <bcl4rk@protonmail.com>
2025-09-15 12:51:07 -0600

    coffeeshop sync

--- experiments/00_basic/main.py
+++ experiments/00_basic/main.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import os
 import json
-# import torch
 
 DATA_DIR = Path(__file__).parent.parent.parent / 'data'
 PROCESSED_DIR = DATA_DIR / 'processed'
 CORPUS_DIR = DATA_DIR / 'corpus'
 
+SPECIAL_TOKENS = {
+    'EOS': '\U000E0000',
+    'PAD': '\U000E0001',
+    'UNK': '\U000E0002',
+}
 
 def setup_corpus_dir():
     if not os.path.exists(CORPUS_DIR):
@@ -20,11 +24,13 @@ def source_texts_iter(text_ids):
             yield f.read()
 
 def build_vocab_json(text_ids, fp):
-    """ saves {char: index} json for all chars in set(src_texts)"""
+    """ saves {char: index} json for all chars in set(src_texts) + SPECIAL_TOKENS"""
     s = set()
     for txt in source_texts_iter(text_ids):
         s.update(set(txt))
     chars = list(s)
+    chars.extend(list(SPECIAL_TOKENS.values()))
+
     vocab_dict = {}
     for i, c in enumerate(chars):
         vocab_dict.update({c: i})
@@ -43,12 +49,20 @@ def get_char_maps(vocab_fp):
     return {v: k for k, v in c2i.items()}, c2i
 
 
+def build_corpus(text_ids, vocab_fp, split_pct=0.9):
+    train_idxs = []
+    val_idxs = []
+    i2c, c2i = get_char_maps(vocab_fp)
+
+    for doc_txt in source_texts_iter(text_ids):
+        tokens = [c2i[char] for char in doc_txt]
+
 
 def main():
     setup_corpus_dir()
     sources = [100] # shakespeare.txt
     vocab_path = CORPUS_DIR / 'vocab.json'
-    # print(build_vocab_json(sources, vocab_path))
+    print(build_vocab_json(sources, vocab_path))
     print(get_char_maps(vocab_path)[0])
     
 
